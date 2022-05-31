5f0e250318a76a761526c0cdef804d785bca5bea
ML6503 <ml6503@gmail.com>
2022-05-31 22:37:03 +0300

    feat: add create fc

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+import * as fs from 'fs/promises';
+import path from 'path';
+
+
+
 export const create = async () => {
-    // Write your code here 
+    const __dirname = path.resolve(path.dirname("src/fs/files/*"));
+
+    let data = 'I am fresh and young';
+     
+       try {
+        await fs.open(path.join(__dirname,'fresh.txt'), 'r')
+        .then(() => {
+           throw new Error('FS operation failed');
+        })
+        .catch((error)=>{
+           if (error.code !== 'ENOENT') {
+            console.error(error);
+           }
+           
+       });
+       
+        await fs.writeFile(path.join(__dirname,'fresh.txt'), data); 
+
+       } catch (e) {
+           console.error(e);
+       }  
 };
+
+create();
