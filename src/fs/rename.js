bb173f09a6a82c17c64c34e2215af09e3696828c
ML6503 <ml6503@gmail.com>
2022-06-04 01:19:53 +0300

    feat: add  rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,32 @@
+import * as fs from 'fs';
+import { existsSync } from 'node:fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesFolder = 'files';
+const oldFilePath = path.join(__dirname, filesFolder, 'wrongFilename.txt');
+const newFilePath = path.join(__dirname, filesFolder, 'properFilename.md');
+  
 export const rename = async () => {
-    // Write your code here 
+  try {
+    fs.stat(oldFilePath, (err, stats) => {
+        if(err) {
+            if(err && err.code === 'ENOENT') {
+                throw new Error('FS operation failed');                
+            }
+            console.error('stat error is: ', err);
+        } else {           
+            if(stats.isFile()) {
+               fs.rename(oldFilePath, newFilePath, (err) => console.error(err));
+               console.log(`File 'wrongFilename.txt' has been renamed to 'properFilename.md' `);
+            } 
+        }
+    });
+  } catch(e) {
+      console.error(e.message);
+  }
 };
+
+rename();
