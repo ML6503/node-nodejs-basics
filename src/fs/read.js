5788f91a06b198836309fbfbe04409af3b252f35
ML6503 <ml6503@gmail.com>
2022-06-04 18:51:53 +0300

    feat: add read fc

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,37 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filesFolder = 'files';
+const filePath = path.join(__dirname, filesFolder, 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+    try {        
+       fs.stat(filePath, (err, stats) => {        
+            if(err) {
+                if(err && err.code === 'ENOENT') {
+                    throw new Error('FS operation failed');                
+                }
+                console.error('Something went wrong');
+            } else { 
+                         
+                if(stats.isFile()) {
+                 fs.readFile(filePath, 'utf8', (err, data) => {
+                        if (err) {
+                            console.error(err);
+                        }
+                        console.log(`File 'fileToRead.txt' has folloiwing content: \n `);
+                        console.info(data);
+                    });                   
+                }
+            }
+        });
+
+    } catch (e) {
+        console.error(e);
+    }
 };
+
+read();
