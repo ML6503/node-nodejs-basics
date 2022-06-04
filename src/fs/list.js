fab5fa92c4664423b599fb5288b50ded292eee74
ML6503 <ml6503@gmail.com>
2022-06-04 18:37:04 +0300

    feat: add list fc

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,30 @@
+import * as fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dirPath = path.join(__dirname, '/files');
+
+
 export const list = async () => {
-    // Write your code here 
+  
+ try {   
+    fs.readdir(dirPath, (err, files) => {        
+        if (err && err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else  if (err) {
+            throw new Error('Something went wrong');
+        } else {
+            console.info('\nFiles in files folder are: \n')
+            files.forEach(file => console.info(file));  
+        }
+    });
+       
+ } catch (e) {
+     console.error(e);
+}
+ 
 };
+
+list();
