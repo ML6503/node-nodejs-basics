10d8f20c8419d93b4b260e55f751a5169206b26c
ML6503 <ml6503@gmail.com>
2022-06-04 22:09:59 +0300

    feat: add parsArgs func

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
 export const parseArgs = () => {
-    // Write your code here 
+    const prefix = 'RSS_';
+    const pattern = new RegExp('^' + prefix);
+    const envs = process.env;
+    let result = '';
+    console.info('\nEnvironment viarables with prefix RSS_: \n ');
+    for(let v in envs) {
+        
+        if (pattern.test(v)) {            
+            result += v +'='+envs[v]+'; ';            
+        }
+    }
+    console.info(result);
+   
 };
+
+parseArgs();
+// function that parses environment variables with prefix RSS_ 
+// and prints them to the console in the format RSS_name1=value1; RSS_name2=value2
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,7 @@
 export const parseEnv = () => {
     // Write your code here 
 };
+
+// function that parses command line arguments
+// (given in format --propName value --prop2Name value2, you don't need to validate it)
+// and prints them to the console in the format propName is value, prop2Name is value2
