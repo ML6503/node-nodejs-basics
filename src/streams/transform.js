73b2431e75e4c0b58f07485afce63761747d9d04
ML6503 <ml6503@gmail.com>
2022-06-05 18:03:20 +0300

    feat: streams funcs

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,24 @@
+import { pipeline, Transform } from'node:stream';
+import { stdin, stdout } from 'process';
+
+ // function that reads data from process.stdin, reverses text using Transform Stream
+ // and then writes it into process.stdout
 export const transform = async () => {
-    // Write your code here 
+    const reversTransform = new Transform({
+        transform: (chunk, _encoding, callback) => callback(null, chunk.toString().split('').reverse().join(''))
+    });
+  pipeline(
+      stdin,
+      reversTransform,
+      stdout,
+      err => {
+          if(err) {
+              console.error('Pipeline got error: ', err);
+          } else {
+              console.info('Pipeline ended.');
+          }
+      }
+  );
 };
+
+transform();
