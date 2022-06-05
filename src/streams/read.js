7a1a718eecd33d0e1b1190c3694363eb4ed22817
ML6503 <ml6503@gmail.com>
2022-06-05 17:02:13 +0300

    refactor: read try catch err

--- src/streams/read.js
+++ src/streams/read.js
@@ -6,11 +6,15 @@ import { stdout } from 'process';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
 
+//  function that reads file fileToRead.txt content using Readable Stream
+// and prints it's content into process.stdout
 export const read = async () => {
     const input = createReadStream(fileToReadPath);
     input.pipe(stdout);
 };
 
-//  function that reads file fileToRead.txt content using Readable Stream
-// and prints it's content into process.stdout
-read();
+try  {
+    read();
+} catch (error) {
+    console.error(error);
+}
