00cff7b0d9156c8992787c120dcbc80ba769f59e
ML6503 <ml6503@gmail.com>
2022-06-05 17:01:29 +0300

    feat: add write func

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import url from 'url';
+import path from 'path';
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+    const input = createReadStream(fileToReadPath);
+    input.pipe(stdout);
 };
+
+//  function that reads file fileToRead.txt content using Readable Stream
+// and prints it's content into process.stdout
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,21 @@
+import path from 'path';
+import { createWriteStream } from 'fs';
+import  url from 'url';
+import { stdin } from 'process';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+const fileToWritePath = path.join(__dirname, 'files', 'fileToWrite.txt');
+
+// function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
 export const write = async () => {
-    // Write your code here 
+    const writeToFile = createWriteStream(fileToWritePath);
+
+    stdin.pipe(writeToFile);  
 };
+
+try  {
+    write();
+} catch (error) {
+    console.error(error);
+}
+
