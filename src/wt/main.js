ac51b15c124eaf09348e0cfe1180bc78dae5b20d
ML6503 <ml6503@gmail.com>
2022-06-06 21:43:47 +0300

    feat: worker thread

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,51 @@
+import  { cpus } from 'os';
+import path from 'path';
+import url from 'url';
+import { Worker, isMainThread } from 'worker_threads';
+
+
+
+// function that creates number of worker threads (equal to the number of host machine logical CPU cores)
+// from file worker.js and able to send data to those threads and to receive result of the computation from them.
 export const performCalculations = async () => {
-    // Write your code here
+    const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+    const fileWorker = path.join(__dirname, 'worker.js');
+    const startNum = 10;
+    const resultsArray = [];
+
+    if(isMainThread) {
+        const cpuNum = cpus().length;
+        const allPromises = []
+
+        for (let i = 0; i < cpuNum; i++) {
+            const promise = new Promise((resolve, reject) => {
+                const worker = new Worker(fileWorker, { workerData: { num: startNum + i } });
+                worker.once('message',  (result) => {                          
+                    resolve({ data : result,  index: i });   
+                });
+            
+                worker.on('error', err => {
+                    console.error(err);
+                    reject({ data : null,  index: i });
+                });
+            });
+ 
+            allPromises.push(promise);      
+        }    
+
+        
+        await Promise.allSettled(allPromises).then((values) => {     
+            
+            values.forEach((val) => {             
+                resultsArray[val.value.index] = { 
+                    status: val.status === 'fulfilled' ? 'resolved' : 'error',
+                    data: val.value.data
+                };            
+            })
+        });          
+    }
+    console.info('FINAL RESULTS ARRAY', resultsArray);
+    return resultsArray;
 };
+
+performCalculations();
